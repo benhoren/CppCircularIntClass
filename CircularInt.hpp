748e2e907c3c46cbf2d3aff1c1a898bcd0eb07c5
Ben Horn <benhorenn@gmail.com>
2018-04-29 21:13:58 +0300

    Update CircularInt.hpp

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -2,19 +2,19 @@
 #include <string>
 #pragma once
 
+std::string NumberToString (int Number );
 
 class CircularInt{
 	public:
-	int number;
 	
+	int number;
 	int start;
 	int end;
 	
-	 CircularInt(int x1, int x2);
-	
+	CircularInt(int x1, int x2);
 	
 	CircularInt operator+ (const CircularInt &ci1);
-	friend CircularInt operator+ ( CircularInt &ci1, const int &x);
+	friend CircularInt operator+ (CircularInt &ci1, const int &x);
 	friend CircularInt operator+ (const int &x ,const CircularInt &ci1);
 	friend CircularInt operator- (const int &x ,const CircularInt &ci1);
 	CircularInt operator* (const int &x);
@@ -22,12 +22,34 @@ class CircularInt{
 	friend CircularInt operator- (const CircularInt &ci1);
 	CircularInt operator- (const int &x);
 	friend CircularInt operator/ (const CircularInt &ci1, const int &x);
+	CircularInt& operator/= (const int &x);
 	CircularInt& operator+= (const int &x);
-	CircularInt &operator++ ();
+	CircularInt& operator++ ();
 	CircularInt operator++ (int);
-	ostream &operator<<(ostream &os );
+	ostream &operator<<(ostream &os);
+	friend ostream &operator<<(ostream &os,  CircularInt &m);
+	CircularInt operator& (const CircularInt &ci1);
+	CircularInt operator| (const CircularInt &ci1);
+	CircularInt& operator-- ();
+	CircularInt operator-- (int);
+	CircularInt& operator-= (const int &x);
 	
-	friend ostream &operator<<(ostream &os ,  CircularInt &m);
+	
+	
+	 bool operator <(const CircularInt& d);
+	 bool operator >(const CircularInt& d);
+	 bool operator <=(const CircularInt& d);
+     bool operator >=(const CircularInt& d);
+	 bool operator ==(const CircularInt& d);
+	 bool operator !=(const CircularInt& d);
+	 
+	 bool operator <(const int& X);
+	 bool operator >(const int& x);
+	 bool operator <=(const int& x);
+     bool operator >=(const int& x);
+	 bool operator ==(const int& x);
+	 bool operator !=(const int& x);
+	
 	
 	
 	void add(int x);
